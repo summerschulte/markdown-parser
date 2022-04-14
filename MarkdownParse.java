dc14e7fb6b3c652ca2951c59e67244c898984299
summerschulte <sschulte@ucsd.edu>
2022-04-14 10:53:48 -0700

    update

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -8,17 +8,30 @@ import java.util.ArrayList;
 public class MarkdownParse {
 
     public static ArrayList<String> getLinks(String markdown) {
+
+        //(https://wrong.com)[otherlink5]
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+            
+        
             int openBracket = markdown.indexOf("[", currentIndex);
+            ///next line doesnt need to be used?
             int closeBracket = markdown.indexOf("]", openBracket);
+    
             int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
+
+            int period = markdown.indexOf(".", openParen);
+
+            int closeParen = markdown.indexOf(")", period);
+              
+            //one solved (when there is () in link)
+            
             toReturn.add(markdown.substring(openParen + 1, closeParen));
+
             currentIndex = closeParen + 1;
-            //System.out.println(currentIndex + "value");
+            
            
         }
 
