5e1058f3d96842090e7d25393dc9338ddde3f10a
summerschulte <sschulte@ucsd.edu>
2022-04-21 09:30:53 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -26,6 +26,27 @@ public class MarkdownParseTest {
         expected.add("some-thing.html");
         
         assertEquals(expected,output);
+
+////test 2 
+        Path fileName1 = Path.of("test1.md");
+        String content1 = Files.readString(fileName1);
+        ArrayList<String> output1 = MarkdownParse.getLinks(content1);
+        ArrayList<String> expected1 = new ArrayList<String>();
+        assertEquals(expected1,output1);
+
+
+//test 3
+        Path fileName2 = Path.of("newmarkdown.md");
+        String content2 = Files.readString(fileName2);
+        ArrayList<String> output2 = MarkdownParse.getLinks(content2);
+        ArrayList<String> expected2= new ArrayList<String>();
+        expected2.add("https://example.com");
+        expected2.add("https://exam(");
+        expected2.add("https://wrong(");
+        System.out.println(output2);
+        
+        assertEquals(expected2,output2);
+
     }
     
 }
